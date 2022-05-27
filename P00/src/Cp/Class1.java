9a6423cecd3833ac219c01474033dd7dbe9b9725
21001078 <21001078@myrp.edu.sg>
2022-05-27 12:27:54 +0800

    first push to P00 repository in Githuh

---
+++ P00/src/Cp/Class1.java
@@ -0,0 +1,28 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: MUHAMMAD ISSA BIN NORMAN
+ * Student ID: 21001078
+ * Class: W64L
+ * Date/Time created: Friday 27-05-2022 12:24
+ */
+
+package Cp;
+
+/**
+ * @author 21001078
+ *
+ */
+public class Class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ P00/test/tea/CT.java
@@ -0,0 +1,61 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: MUHAMMAD ISSA BIN NORMAN
+ * Student ID: 21001078
+ * Class: W64L
+ * Date/Time created: Friday 27-05-2022 12:25
+ */
+
+package tea;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 21001078
+ *
+ */
+class CT {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
